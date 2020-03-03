ebf3c81b752facd369e900f5bb805c2a63200ca7
Sudeepthi <Sudeepthi@DESKTOP-MA4EI5I.mshome.net>
2020-03-03 12:25:19 +0530

    Initial

---
+++ Intersectnconc/src/main/java/com/maven/Intersectnconc/Simpleinterest.java
@@ -0,0 +1,16 @@
+package com.maven.Intersectnconc;
+class Simpleinterest{
+	double principalamount;
+	double years;
+	double rateofinterestpyear;
+	Simpleinterest(double principalamount, double  years, double rateofinterestpyear){
+		this.principalamount = principalamount;
+		this.years = years;
+		this.rateofinterestpyear = rateofinterestpyear;
+		}	
+	public double findsimpleinterest(){
+		double result = (principalamount * years * rateofinterestpyear);
+		result /= 100;
+		return result;
+	}
+}
